a4921ec6d24e594093a64d8b0d85d9a6d35cb16b
leaf_8742 <leaf_8742@163.com>
2021-09-26 09:34:39 +0800

    创建了一个新类: HelloServlet

---
+++ src/com/mobiletrain/web/HelloServlet.java
@@ -0,0 +1,11 @@
+package com.mobiletrain.web;
+
+import javax.servlet.HttpServlet;
+
+public class HelloServlet implements HttpServlet {
+	public void doGet(HttpServletReqeust request, HttpServletResponse response) {
+	}
+	
+	public void doPost(HttpServletReqeust request, HttpServletResponse response) {
+	}
+}
